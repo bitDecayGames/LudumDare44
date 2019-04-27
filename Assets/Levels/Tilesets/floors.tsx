7a11abc1223168134bb22542ef02268b0e45af92
Logan Moore <monday.hopscotch@gmail.com>
2019-04-27 10:36:36 -0600

    initial tiles + meta

---
+++ Assets/Levels/Tilesets/floors.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="FloorTiles" tilewidth="16" tileheight="32" tilecount="15" columns="5">
+ <image source="../../Art/Tilesets/Floors/FloorTiles.png" width="80" height="96"/>
+ <tile id="0">
+  <properties>
+   <property name="height" value="0"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="height" value="0"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="height" value="0"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="height" value="0"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="height" value="0"/>
+  </properties>
+ </tile>
+ <tile id="5">
+  <properties>
+   <property name="height" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="height" value="1"/>
+  </properties>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="height" value="1"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="height" value="1"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="height" value="1"/>
+  </properties>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="height" value="2"/>
+  </properties>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="height" value="2"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="height" value="2"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="height" value="2"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="height" value="2"/>
+  </properties>
+ </tile>
+</tileset>
