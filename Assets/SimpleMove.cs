02e33b23067e644bf104c7e6cfe22d282d07e506
mwingfield <mwingfie@gmail.com>
2019-04-27 17:11:17 -0600

    fixed board movement; hopefully commiting this bank meta doesn't cause problems

--- Assets/SimpleMove.cs
+++ Assets/SimpleMove.cs
@@ -1,32 +1,45 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using Board;
 using UnityEngine;
 
-public class SimpleMove : MonoBehaviour
-{
-
+public class SimpleMove : MonoBehaviour {
     private BoardPosition boardPos;
+
+    private BoardManager board;
+    private Board.Board.Occupier occupier;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         boardPos = GetComponent<BoardPosition>();
+        occupier = GetComponent<Board.Board.Occupier>();
+        board = FindObjectOfType<BoardManager>();
+        
+        Debug.Log(board.board);
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.A))
-            GetComponent<BoardPosition>().
-                boardPos.x -= 1;
-        if (Input.GetKeyDown(KeyCode.D))
-            GetComponent<BoardPosition>().
-                boardPos.x += 1;
-        if (Input.GetKeyDown(KeyCode.W))
-            GetComponent<BoardPosition>().
-                boardPos.y -= 1;
-        if (Input.GetKeyDown(KeyCode.S))
-            GetComponent<BoardPosition>().
-                boardPos.y += 1;
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            if (board.board.MoveLeft(occupier)) {
+                GetComponent<BoardPosition>().boardPos.x -= 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.D)) {
+            if (board.board.MoveRight(occupier)) {
+                GetComponent<BoardPosition>().boardPos.x += 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.W)) {
+            if (board.board.MoveDown(occupier)) {
+                GetComponent<BoardPosition>().boardPos.y -= 1;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.S)) {
+            if (board.board.MoveUp(occupier)) {
+                GetComponent<BoardPosition>().boardPos.y += 1;
+            }
+        }
     }
 }
