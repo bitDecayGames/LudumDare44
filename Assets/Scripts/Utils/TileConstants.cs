7c483cf3eebac64beb17e55d08072b75d9ae2fa3
Logan Moore <monday.hopscotch@gmail.com>
2019-04-27 14:44:45 -0600

    rig up initial player sprite, animations tbd

--- Assets/Scripts/Utils/TileConstants.cs
+++ Assets/Scripts/Utils/TileConstants.cs
@@ -2,5 +2,8 @@ namespace Utils {
     public static class TileConstants {
         public static int TILE_WIDTH = 16;
         public static int TILE_HEIGHT = 8;
+
+        // Used to map untiy coordinates to board coordinates
+        public static int TILE_SIZE = 8;
     }
 }
