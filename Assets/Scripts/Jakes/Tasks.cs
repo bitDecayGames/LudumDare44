cd74bcdac3b446c1527844a64f8d62d1b5523770
Jacob Caban-Tomski <jacque006@gmail.com>
2019-04-27 12:15:04 -0600

    Initial task stuff

---
+++ Assets/Scripts/Jakes/Tasks.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tasks : MonoBehaviour
+{
+    List<Task> currentTasks;
+
+
+    void Start()
+    {
+        currentTasks = new List<Task>();
+    }
+    
+    void Update()
+    {
+        
+    }
+}
