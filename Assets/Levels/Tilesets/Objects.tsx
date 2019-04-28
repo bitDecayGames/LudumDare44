cfb8bda0a57054180ba4c4baeb7459387a154b62
unknown <zombiefish90@gmail.com>
2019-04-28 08:28:12 -0600

    Fixing the tiled bullshiters 	modified:   Assets/Levels/JakeTest.tmx 	modified:   Assets/Levels/JakeTest.tmx.meta 	modified:   Assets/Levels/LoganTest1.tmx.meta 	modified:   Assets/Levels/Tilesets.meta 	modified:   Assets/Levels/Tilesets/Objects.tsx 	modified:   Assets/Levels/Tilesets/Objects.tsx.meta 	modified:   Assets/Levels/Tilesets/floors.tsx 	modified:   Assets/Levels/Tilesets/floors.tsx.meta

--- Assets/Levels/Tilesets/Objects.tsx
+++ Assets/Levels/Tilesets/Objects.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Objects" tilewidth="16" tileheight="32" tilecount="65" columns="13">
- <image source="../../Art/Tilesets/Objects/Objects.png" width="208" height="160"/>
+<tileset version="1.2" tiledversion="1.2.1" name="Objects" tilewidth="16" tileheight="32" tilecount="78" columns="13">
+ <image source="../../Art/Tilesets/Objects/Objects.png" width="208" height="192"/>
  <tile id="8">
   <properties>
    <property name="stepName" value="CashRegister"/>
