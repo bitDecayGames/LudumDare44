6d2b83ac005451a7fa27324e6af022ecbd298299
Tanner <Shadoninja@gmail.com>
2019-04-29 00:15:50 -0600

    ye

--- Assets/Scripts/Managers/FeedbackManager.cs
+++ Assets/Scripts/Managers/FeedbackManager.cs
@@ -7,11 +7,19 @@ namespace Managers {
         public TextMeshPro NegativeFeedbackTextPrefab;
 
         public void Positive(string text, Transform transform) {
+            if (transform.Find("Sprite") != null)
+            {
+                transform = transform.Find("Sprite").transform;
+            }
             var t = Instantiate(PositiveFeedbackTextPrefab, transform);
             t.text = text;
         }
 
         public void Negative(string text, Transform transform) {
+            if (transform.Find("Sprite") != null)
+            {
+                transform = transform.Find("Sprite").transform;
+            }
             var t = Instantiate(NegativeFeedbackTextPrefab, transform);
             t.text = text;
         }
