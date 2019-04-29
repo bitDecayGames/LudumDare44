6894d87ed00de61d70914835043b423943873c27
Tanner <Shadoninja@gmail.com>
2019-04-29 15:25:57 -0600

    ye

--- Assets/Levels/Tilesets/floors.tsx
+++ Assets/Levels/Tilesets/floors.tsx
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="FloorTiles" tilewidth="16" tileheight="32" tilecount="25" columns="5">
+<tileset version="1.2" tiledversion="1.2.3" name="FloorTiles" tilewidth="16" tileheight="32" tilecount="35" columns="5">
  <grid orientation="isometric" width="16" height="32"/>
- <image source="../../Art/Tilesets/Floors/FloorTiles.png" width="80" height="160"/>
+ <image source="../../Art/Tilesets/Floors/FloorTiles.png" width="80" height="224"/>
  <tile id="0">
   <properties>
    <property name="height" value="0"/>
