2b15c329e6da6a0ee73d4b4621ff28450ff82cec
nandemoguy <nandemoguy@gmail.com>
2019-04-28 23:37:14 -0600

    actual tile sets

---
+++ Assets/Art/Tilesets/Cars/Cars.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Cars" tilewidth="16" tileheight="32" tilecount="54" columns="18">
+ <image source="Cars.png" width="288" height="96"/>
+</tileset>
---
+++ Assets/Art/Tilesets/Objects/Objects.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Objects" tilewidth="16" tileheight="32" tilecount="117" columns="13">
+ <image source="Objects.png" width="208" height="288"/>
+</tileset>
