b17ed457d8e5403393490166a0c4359945d5a7f5
mwingfield <mwingfie@gmail.com>
2019-05-04 20:35:29 -0600

    added some gifs

---
+++ docs/make-four-walking-gifs.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir tmp
+convert raw/*walk*.png -crop 16x32 tmp/frame.png
+
+sheets=""
+for i in {0..3}
+do
+	startingFrame=`expr $i \* 32`
+   	for k in {0..3}
+	do
+		frame0=`expr $startingFrame + $k + 0 \* 8`
+		frame1=`expr $startingFrame + $k + 1 \* 8`
+		frame2=`expr $startingFrame + $k + 2 \* 8`
+		frame3=`expr $startingFrame + $k + 3 \* 8`
+		sheetNumber=`expr $i \* 4 + $k`
+		montage "tmp/frame-$frame0.png" "tmp/frame-$frame1.png" "tmp/frame-$frame2.png" "tmp/frame-$frame3.png" -tile x1 -geometry 16x32+0+0 "tmp/sheet-$sheetNumber.png"
+		sheets="$sheets tmp/sheet-$sheetNumber.png"
+	done
+done 
+convert -loop 0 -delay 20 $sheets final/four-characters.gif
+rm -r tmp
---
+++ docs/make-sheet-example.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+mkdir tmp
+
+sheets=""
+for i in {0..3}
+do
+	sheets="$sheets raw/sheet-$i.png"
+done 
+montage $sheets -tile 1x -geometry 128x32+0+0 "tmp/tmp-sheet-0.png"
+sheets=""
+for i in {4..7}
+do
+	sheets="$sheets raw/sheet-$i.png"
+done 
+montage $sheets -tile 1x -geometry 128x32+0+0 "tmp/tmp-sheet-1.png"
+
+montage tmp/tmp-sheet-0.png tmp/tmp-sheet-1.png -tile x1 -geometry 128x128+10+0 "final/sheet-example.png"
+rm -r tmp
